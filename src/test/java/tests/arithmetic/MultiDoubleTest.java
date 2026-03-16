c7d6eaa91d736b7c56cfebbeb858916782279810
Alexis Alejandro Calderon <alejandrodev2019@gmail.com>
2026-03-15 23:15:54 -0300

    refactor on based of review's mentor, data provider

--- src/test/java/tests/arithmetic/MultiDoubleTest.java
+++ src/test/java/tests/arithmetic/MultiDoubleTest.java
@@ -10,6 +10,6 @@ public class MultiDoubleTest extends BaseTest {
     @Test(dataProvider = "multiDoubleData", dataProviderClass = CalculatorDataProvider.class, groups = {"regression"})
     public void verifyMultiDouble(double a, double b, double expected) {
         double result = calculator.mult(a, b);
-        Assert.assertEquals(result, expected, 0.0001, "Multiplication double result incorrect");
+        Assert.assertEquals(result, expected, "Multiplication double result incorrect");
     }
 }
